5e16df83cae681c9d6061e0ce363f57fefed3e4d
Phaiax <phaiax@invisibletower.de>
2014-01-25 21:17:23 +0100

    First Level Structure

---
+++ Assets/Scripts/IntroScripts/NextSceneWithKey.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextSceneWithKey : MonoBehaviour {
+
+	public KeyCode ContinueKey;
+	public string NextLevelName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (ContinueKey)) {
+			Application.LoadLevel(NextLevelName);
+		}
+	}
+}
