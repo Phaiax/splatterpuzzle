9ee39d21ce3e26db1edff47ba6c6c95bea511717
tepelmann <JanTepelmann@web.de>
2014-01-26 13:16:55 +0100

    GameManager added for checking Winning Condition

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject Player;
+	public GameObject Goal;
+	public float GoalArrivedDelta;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = Vector2.Distance(Player.transform.position, Goal.transform.position);
+		if (distance < GoalArrivedDelta)
+			Debug.Log ("You win!");
+		else
+			Debug.Log ("...");
+	}
+}
