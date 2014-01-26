905a993d793875d5179294d17d1df91e39f05086
tepelmann <JanTepelmann@web.de>
2014-01-26 11:50:43 +0100

    Playersprite and ..

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+	public KeyCode Up;
+	public KeyCode Down;
+	public KeyCode Right;
+	public KeyCode Left;
+
+	public float Speed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 currentSpeed = Vector2.zero;
+		if (Input.GetKey (Up))
+			currentSpeed.y = Speed;
+		if (Input.GetKey (Down))
+			currentSpeed.y = -Speed;
+		if (Input.GetKey (Left))
+			currentSpeed.x = -Speed;
+		if (Input.GetKey (Right))
+			currentSpeed.x = Speed;
+		rigidbody2D.velocity = currentSpeed;
+	}
+}
