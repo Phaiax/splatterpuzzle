f8d23513d764cc3169af3b06f9e041294668677f
tepelmann <JanTepelmann@web.de>
2014-01-27 05:26:37 +0100

    Explosions added

---
+++ Assets/Scripts/AllExplosions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class AllExplosions  {
+	
+	public static void Start () {
+		GameObject[] explosions = GameObject.FindGameObjectsWithTag ("Explosion");
+		foreach (GameObject go in explosions) {
+			((ParticleSystem)go.GetComponent("ParticleSystem")).Stop();
+			((ParticleSystem)go.GetComponent("ParticleSystem")).Play();
+		}
+	}
+}
